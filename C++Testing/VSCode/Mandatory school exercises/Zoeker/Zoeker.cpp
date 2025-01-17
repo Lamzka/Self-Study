f05984611fbb4a659ad7eb89f87b2616a0ed4b74
Lamzka <Raymond0besson@Gmail.com>
2025-01-17 15:26:18 +0100

    added comments to zoeker

--- C++Testing/VSCode/Mandatory school exercises/Zoeker/Zoeker.cpp
+++ C++Testing/VSCode/Mandatory school exercises/Zoeker/Zoeker.cpp
@@ -7,75 +7,70 @@
 
 using namespace std;
 
-
 void SeperateInput();
 
+//List with Inputs that has to be decoded
+list<string> Input{ "maan", "zon", "3", "ster", "zon", "planeten", "2", "zon", "maan", "4", "ster", "3", "kompas", "5", "zon", "maan", "planeten", "6", "ster", "7", "maan", "zon", "planeten", "8", "maan", "ster", "zon", "9", "planeten", "10", "maan", "zon", "11", "ster", "12", "zon", "13" };
 
-list<string> scripts{ "maan", "zon", "3", "ster", "zon", "planeten", "2", "zon", "maan", "4", "ster", "3", "kompas", "5", "zon", "maan", "planeten", "6", "ster", "7", "maan", "zon", "planeten", "8", "maan", "ster", "zon", "9", "planeten", "10", "maan", "zon", "11", "ster", "12", "zon", "13" };
 
-map<string, int> Input;
+map<string, int> StoredInputs; //stores a string and its value
 
-int currentCount = 10;
 
-long long CurrentAmount;
+int currentCount = 10; //current count for next new value
 
-bool TryParse(const string& str, int& result)
+
+long long CurrentAmount; //output
+
+
+bool TryParse(const string& str, int& result) //checks if string has a number and returns the number
 {
-
-	try
+	try //try to convert string to int
 	{
 		result = stoi(str);
 
 		return true;
 	}
-	catch (invalid_argument)
+	catch (invalid_argument) //if string is not a number return false
 	{
 		return false;
 	}
-	catch (out_of_range)
+	catch (out_of_range) //if string is not a number return false
 	{
 		return false;
 	}
 	
 }
 
+//main
 int main()
 {
 	SeperateInput();
 	cout << CurrentAmount << "\n";
 }
 
-void SeperateInput()
+
+void SeperateInput() 
 {
 	int i;
-	
-
-	for (string script : scripts)
+	for (string script : Input)
 	{
-		if (TryParse(script, i))
+		if (TryParse(script, i)) //if the string is/has a number: multiply with CurrentAmound
 		{
 			CurrentAmount = (CurrentAmount * i);
-			
 		}
-		else if (!Input.count(script))
+		else if (!StoredInputs.count(script))  //if the string is not stored in StoredInputs: add it to StoredInputs with a value of currentCount
 		{
-			Input.insert({ script, currentCount });
+			StoredInputs.insert({ script, currentCount });
 
-			CurrentAmount += Input[script];
+			CurrentAmount += StoredInputs[script]; //add the value of the string to CurrentAmount
 
-			currentCount += 10;
-	
+			currentCount += 10; //increment currentCount by 10
 		}
-		else if (Input.count(script))
+		else if (StoredInputs.count(script)) //if the string is stored in StoredInputs: add the value of the string to CurrentAmount
 		{
-			CurrentAmount += Input[script];
-
-			
+			CurrentAmount += StoredInputs[script];
 		}
-
-		
 	}
-	
 }
 
 
